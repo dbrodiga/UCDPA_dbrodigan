00d9dce448a8d45ef6e4b41632d9e9b0a5a8d248
DBrodiga <david.brodigan@aig.com>
2021-07-15 22:48:33 +0100

    Duplicates and missing values

--- Project.py
+++ Project.py
@@ -1,1 +1,6 @@
-print("Hello2")
+import pandas as pd
+import matplotlib.pyplot as plt
+
+property_price = pd.read_csv('PPR_ALL.csv', encoding = 'latin1')
+
+print(property_price.head())
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-# This is a sample Python script.
+# Import required
+import requests
+import pandas as pd
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Retrieving data from an online API
+request = requests.get('https://www.alphavantage.co/query?function=OVERVIEW&symbol=AIG&apikey=2KMJ76XV3WQ5M9K7')
 
+data = request.json()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print('The Book Value of AIG is ' + data['BookValue'] + ' and the Earnings per share figure is ' + data['EPS'] + '.')
 
+# Importing a CSV file into a Pandas DataFrame
+prop_price = pd.read_csv('PPR_ALL.csv', encoding='latin1', parse_dates=['Date of Sale (dd/mm/yyyy)'])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(prop_price.head())
+print(prop_price.info())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Remove duplicates
+prop_price.drop_duplicates(inplace=True)
+
+print(prop_price.info())
+
+# Check for missing values
+print(prop_price.isna().any())
+
+# Remove the columns Postal Code and Property Size Description as there are so many missing values
+# No accurate insights can be sought on those variables
+# The below code could have filled in the missing values with a zero but this would add no benefit to the analysis
+# prop_price.fillna(0, inplace=True)
+prop_price.drop(['Postal Code', 'Property Size Description'], axis='columns', inplace=True)
+
+print(prop_price.info())
+
+# Replace Irish with English in Description of Property
+irish = ['Teach/?ras?n C?naithe Nua', 'Teach/Árasán Cónaithe Nua', 'Teach/Árasán Cónaithe Atháimhe']
+english = ['New Dwelling house /Apartment', 'New Dwelling house /Apartment', 'Second-Hand Dwelling house /Apartment']
+prop_price['Description of Property'] = prop_price['Description of Property'].replace(irish, english)
